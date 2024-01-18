ced541c2853d5b0af1602ef9c564555c41779c37
raghavnallana17298 <112153179+raghavnallana17298@users.noreply.github.com>
2024-01-18 13:50:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const hamburger = document.getElementById('hamburger'); 
+const menu = document.querySelector('.menu'); 
+
+hamburger.addEventListener('click', function () { 
+	const hamIcon = this.querySelector('.hamburger-icon'); 
+	const crossIcon = this.querySelector('.cross-icon'); 
+	if (hamIcon.style.display === "none") { 
+		hamIcon.style.display = "inline-block"
+		menu.style.display = "none"
+		crossIcon.style.display = "none"
+	} 
+	else { 
+		crossIcon.style.display = "inline-block"
+		hamIcon.style.display = "none"
+		menu.style.display = "block"
+	} 
+});
